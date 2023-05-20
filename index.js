2803dec975467064103e4cef62158f2767544794
Sohel-Developer <sohel.webdevoloper.com>
2023-05-20 15:00:42 +0600

    Single Toy Load In MongoDB

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 /* Midelware */
@@ -48,6 +48,17 @@ async function run() {
 
             const result = await toysCollection.find().toArray()
             res.send(result)
+
+        })
+
+
+        app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+
+            const toy = await toysCollection.findOne(query);
+            res.send(toy)
+
 
         })
 
