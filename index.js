d040b0c4a49d60831763b81ce098dcf17debad59
Sohel-Developer <sohel.webdevoloper.com>
2023-05-21 17:10:02 +0600

    Deleted Method added

--- index.js
+++ index.js
@@ -83,6 +83,14 @@ async function run() {
         })
 
 
+        app.delete('/mytoys/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await toysCollection.deleteOne(query);
+            res.send(result)
+        })
+
+
 
 
         // Send a ping to confirm a successful connection
